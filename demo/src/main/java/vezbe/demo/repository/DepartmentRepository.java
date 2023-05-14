be14b54f33a3d810e7713ed88f9ef92eb771a02a
Sara Sapundzija <sara.sapundzija@gmail.com>
2023-05-14 16:57:41 +0200

    Revert "valjda dobro"
    This reverts commit 0e6104ed6d4e8208a8455fca13b4d517239c3437.

--- demo/src/main/java/vezbe/demo/repository/DepartmentRepository.java
+++ demo/src/main/java/vezbe/demo/repository/DepartmentRepository.java
@@ -4,9 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vezbe.demo.model.Department;
 
-import java.util.List;
-
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    List<Department> findByEmployees(Long employeeId);
 }
--- demo/src/main/java/vezbe/demo/repository/EmployeeRepository.java
+++ demo/src/main/java/vezbe/demo/repository/EmployeeRepository.java
@@ -2,13 +2,9 @@ package vezbe.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import vezbe.demo.model.Department;
 import vezbe.demo.model.Employee;
-
-import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Employee getByUsername(String username);
-
 }
